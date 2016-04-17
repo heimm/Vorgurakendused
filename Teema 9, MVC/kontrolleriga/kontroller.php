28a688766b941a746c0dea08c1d179c68f2dd3a4
heimm <heimm@github.com>
2016-04-17 17:59:14 +0300

    Üheksas teema

---
+++ Teema 9, MVC/kontrolleriga/kontroller.php
@@ -0,0 +1,28 @@
+﻿<?php
+require_once('head.html');
+  $pildiMassiiv = array("pildid/nameless1.jpg",
+  "pildid/nameless2.jpg",
+  "pildid/nameless3.jpg",
+  "pildid/nameless4.jpg",
+  "pildid/nameless5.jpg",
+  "pildid/nameless6.jpg");
+$page = "";
+if (!empty($_GET['page'])) {
+  $page = $_GET['page'];
+}
+switch($page){
+	case "galerii":
+		include("galerii.html");
+	break;
+	case "vote":
+		include("vote.html");
+	break;
+	case "tulemus":
+		include("tulemus.html");
+	break;
+	default:
+		include("pealeht.html");
+	break;
+}
+require_once('foot.html');
+?>
