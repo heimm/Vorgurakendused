2f505d701ace294836328b72f822509ada0f3ca9
heimm <heimm@github.com>
2016-04-17 17:51:48 +0300

    Üheksas teema

---
+++ Teema 9, MVC/tulemus1.php
@@ -0,0 +1,27 @@
+﻿<?php
+require_once('head.html');
+?>
+	<h3>Valiku tulemus</h3>
+	<p>Siia tuleb valiku tulemus, mida saab kuvada ainult PHP abil :)</p>
+	<?php
+	$pildiMassiiv = array("pildid/nameless1.jpg",
+	"pildid/nameless2.jpg",
+	"pildid/nameless3.jpg",
+	"pildid/nameless4.jpg",
+	"pildid/nameless5.jpg",
+	"pildid/nameless6.jpg");
+	if(isset($_GET['pilt'])){
+		echo "PILT VALITUD";
+		$value = $_GET['pilt'];
+		foreach ($pildiMassiiv as $key => $value) {
+			if ($value == $key) {
+				echo "<br>";
+				echo "Pilt on andmebaasis";
+			}
+		}
+		}
+	if (empty($_GET['pilt'])) {
+		echo "PALUN VALI PILT!";
+	}
+	require_once('foot.html')
+	?>
